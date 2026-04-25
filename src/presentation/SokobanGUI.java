59aab44407eec20a04a59c0506eae5b371532cf3
Oscar10lm <oscarlasso10u@gmail.com>
2026-04-25 17:23:22 -0500

    prepareActions

--- src/presentation/SokobanGUI.java
+++ src/presentation/SokobanGUI.java
@@ -1,31 +1,32 @@
+package presentation;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 
-
 /**
- * Clase de GUI (Interfaz gráfica para el juego EasySkoban
+ * Clase de GUI (Interfaz gráfica para el juego EasySokoban
  *
  * @author gaitan-lasso
  * @version 16/04/2026
  */
-public class SokobanGUI extends JFrame{
-
+public class SokobanGUI extends JFrame {
 
     /**
      * Constructor para la interfáz donde se coloca el titulo
      */
-    public SokobanGUI(String title)
-    {   
-        //Pasa el titulo
+    public SokobanGUI(String title) {
+        // Pasa el titulo
         super(title);
-        
+
         prepareElements();
         prepareActions();
-        
     }
 
+    /**
+     * Configura los oyentes de eventos de la ventana.
+     */
     public void prepareActions() {
         WindowListener oyenteCierre = new WindowAdapter() {
             @Override
@@ -35,72 +36,51 @@ public class SokobanGUI extends JFrame{
         };
 
         this.addWindowListener(oyenteCierre);
-
     }
 
+    /**
+     * Muestra un diálogo de confirmación antes de cerrar la aplicación.
+     */
     public void confirmarSalida() {
-
         int respuesta = JOptionPane.showConfirmDialog(
             this,
-            "¿Estas 99.999999 infinito seguro?",
-            "Confirmación de salida",
+            "¿Esta seguro de que desea salir de EasySokoban?",
+            "Confirmacion de Salida",
             JOptionPane.YES_NO_OPTION,
             JOptionPane.QUESTION_MESSAGE
         );
 
         if (respuesta == JOptionPane.YES_OPTION) {
-            System.out.println("Pero muuuy seguro?");
-            int respuesta2 = JOptionPane.showConfirmDialog(
-                this,
-                "No ya enserio",
-                "Confirmación de salida segurisima",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE
-            );
-
-            if (respuesta2 == JOptionPane.YES_OPTION) {
-                System.out.println("Ya te extraño :(");
-                System.exit(0);
-            }
-
-            if (respuesta2 == JOptionPane.NO_OPTION) {
-                System.out.println("Lo sabia mi amOoOoooooor");
-            }
-        }
-
-        if (respuesta == JOptionPane.NO_OPTION) {
-            System.out.println("Te la creiste puta!!!!!!!");
             System.exit(0);
         }
-
-
-
     }
-    
-    public void prepareElements(){
-        
+
+    /**
+     * Configura el tamaño y posición inicial de la ventana.
+     * La ventana ocupa un cuarto de la pantalla (mitad del ancho × mitad del alto)
+     * y queda centrada.
+     */
+    public void prepareElements() {
         Toolkit miPantalla = Toolkit.getDefaultToolkit();
         Dimension tamanoPantalla = miPantalla.getScreenSize();
-        
+
         int anchoPantalla = tamanoPantalla.width;
         int altoPantalla = tamanoPantalla.height;
 
-      
         int anchoVentana = anchoPantalla / 2;
         int altoVentana = altoPantalla / 2;
-        
+
         this.setSize(anchoVentana, altoVentana);
-        
         this.setLocationRelativeTo(null);
-
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        
     }
-    
-    
-    public static void main (String [] args)
-    {
+
+    /**
+     * Punto de entrada principal para ejecutar la aplicación desde consola.
+     *
+     * @param args argumentos de la línea de comandos (no utilizados)
+     */
+    public static void main(String[] args) {
         SokobanGUI ventana = new SokobanGUI("EasySokoban");
         ventana.setVisible(true);
     }
